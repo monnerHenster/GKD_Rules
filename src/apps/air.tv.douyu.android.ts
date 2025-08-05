efe97adc1a22e9ba29bd2d3e10fd6740dc079f3c
monnerHenster <monkeyxox88@gmail.com>
2025-08-05 22:03:51 +0800

    Update air.tv.douyu.android.ts

--- src/apps/air.tv.douyu.android.ts
+++ src/apps/air.tv.douyu.android.ts
@@ -17,5 +17,24 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+  id: 'air.tv.douyu.android',
+  name: '斗鱼',
+  groups: [
+    {
+      key: 1,
+      name: '[ChangeMe]规则名称-2025-08-05 22:00:37',
+      desc: '[ChangeMe]本规则由GKD网页端审查工具生成',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.douyu.module.home.pages.main.MainActivity',
+          matches: '[vid="ahb"]',
+          snapshotUrls: 'https://i.gkd.li/i/21601453',
+        },
+      ],
+    },
+  ],
+},
   ],
 });
